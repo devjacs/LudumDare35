bdb3d17e252772cdd33e1c392733e25a11471b15
Jacs <developer.jacs@gmail.com>
2016-04-17 11:12:16 +1200

    People spawn around the player with random colours, end of the timer will make a person disappear and the evil person gets a new colour

--- LudumDare35/Assets/Scripts/GameManager.cs
+++ LudumDare35/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour {
 
@@ -10,12 +11,24 @@ public class GameManager : MonoBehaviour {
 	private int blinkInterval = 3;
 	private float nextSecond = 0;
 
+	//temp variables
+	public Material[] capsuleColours = new Material[2]; //red & blue
+	public GameObject[] people = new GameObject[12];
+	private int evilPerson;
+
 	public Text countdownText;
 
 
 	// Use this for initialization
 	void Start () {
 		RestartGame();
+		//set the players positions..
+		int radius = 5;
+		for(int i = 1; i <= 12; i++) {
+			float x = radius * Mathf.Cos(Mathf.PI / -2 + (2 * i * Mathf.PI) / 12);
+			float z = radius * Mathf.Sin(Mathf.PI / -2 + (2 * i * Mathf.PI) / 12);
+			people[i-1].transform.position = new Vector3(x, 1.25f, z);
+		}
 	}
 
 	public void RestartGame() {
@@ -23,6 +36,31 @@ public class GameManager : MonoBehaviour {
 		secondsLeftInRound = 20;
 		nextSecond = 0;
 		countdownText.text = secondsLeftInRound.ToString();
+		evilPerson = Random.Range(0, people.Length);
+		foreach (GameObject person in people) {
+			int materialIndex = Random.Range(0, 2);
+			person.GetComponent<PersonFeatures>().SetColour(capsuleColours[materialIndex]);
+		}
+	}
+
+	void UpdateBadGuy() {
+		/******** Update Bad Guys appearance *********/
+		//they have to change and can't be the same as the previous turn..
+		Material currentMaterial = people[evilPerson].GetComponent<PersonFeatures>().GetColour();
+		int materialIndex = Random.Range(0, 2);
+		while (capsuleColours[materialIndex] == currentMaterial) {
+			materialIndex = Random.Range(0, 2);
+		}
+		people[evilPerson].GetComponent<PersonFeatures>().SetColour(capsuleColours[materialIndex]);
+
+
+
+		/******** Remove one player **********/
+		int personToKill = Random.Range(0, people.Length);
+		while (personToKill == evilPerson || !people[personToKill].GetComponent<PersonFeatures>().GetEnabled()) {
+			personToKill = Random.Range(0, people.Length);
+		}
+		people[personToKill].GetComponent<PersonFeatures>().SetEnabled(false);
 	}
 	
 	// Update is called once per frame
@@ -39,6 +77,8 @@ public class GameManager : MonoBehaviour {
 				secondsLeftInRound = 0;
 				countdownText.color = Color.red;
 				nextSecond += blinkInterval;
+				//when the screen is black
+				UpdateBadGuy();
 			} else {
 				secondsLeftInRound--;
 				nextSecond += secondsInterval;
---
+++ LudumDare35/Assets/Scripts/PersonFeatures.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonFeatures : MonoBehaviour {
+
+	private Material colour;
+	private Renderer renderer;
+	private bool enabled = true;
+
+	void Start() {
+		renderer = GetComponent<Renderer>();
+		renderer.enabled = true;
+	}
+
+	public void SetColour(Material colour) {
+		this.colour = colour;
+		renderer.sharedMaterial = colour;
+	}
+
+	public Material GetColour() {
+		return colour;
+	}
+
+	public bool GetEnabled() {
+		return enabled;
+	}
+
+	public void SetEnabled(bool enabled) {
+		this.enabled = enabled;
+		renderer.enabled = enabled;
+	}
+}
