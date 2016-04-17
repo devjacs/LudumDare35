a26b49dd9a9c3c9edd5f9c983f80876f1da84338
Alex McNeill <mcnear1@student.op.ac.nz>
2016-04-17 14:40:02 +1200

    Add flicker script for fire

---
+++ LudumDare35/Assets/Campfire Pack/Script/Flicker.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flicker : MonoBehaviour {
+
+	private Light light;
+	private Color originalColor;
+	private float timePassed;
+	private float changeValue;
+
+	void Start()
+	{
+		light = GetComponent<Light> ();
+
+		if (light != null) 
+		{
+			originalColor = light.color;
+		} 
+		else 
+		{
+			enabled = false;
+			return;
+		}
+
+		changeValue = 0;
+		timePassed = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timePassed = Time.time;
+		timePassed = timePassed - Mathf.Floor (timePassed);
+		light.color = originalColor * CalculateChange ();
+	}
+
+	private float CalculateChange()
+	{
+		changeValue = -Mathf.Sin (timePassed * 12 * Mathf.PI) * 0.05f + 0.95f;
+		return changeValue;
+	}
+}
